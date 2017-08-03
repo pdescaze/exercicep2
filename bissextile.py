0aafb3d0a594f1ccb3c52da2700ac9bfc4fcf58b
Pierre Descazeaux <descazeaux.pierre@gmail.com>
2017-08-03 17:09:49 +0200

    modification espace et orthographe

--- bissextile.py
+++ bissextile.py
@@ -1,15 +1,15 @@
-annee=input("Saissez une année: )
+annee = input ("Saisissez une année: )
 
 try:
-    annee=int(annee)
+    annee = int(annee)
     assert annee>0
 except ValueError:
-    print("Vous n'avez pas saisie de nombre.")
+    print ("Vous n'avez pas saisie de nombre.")
 except AssertionError:
-    print("L'année saisie est inférieure ou égale à 0")
+    print ("L'année saisie est inférieure ou égale à 0")
 
-if annee%400 == 0 or (annee%4 == 0 and annee%100 !=0):
-    print("L'année saisie est bissextile.")
+if annee % 400 == 0 or (annee % 4 == 0 and annee % 100 !=0):
+    print ("L'année saisie est bissextile.")
 else:
-    print("L'année saisie n'est pas bissextile.")
+    print ("L'année saisie n'est pas bissextile.")
 
