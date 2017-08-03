876ee6b9f6083f1e42e7bab078ab0b2dd8168e37
Pierre Descazeaux <descazeaux.pierre@gmail.com>
2017-08-03 17:08:37 +0200

    Ajout du script .py bissextile

---
+++ bissextile.py
@@ -0,0 +1,15 @@
+annee=input("Saissez une année: )
+
+try:
+    annee=int(annee)
+    assert annee>0
+except ValueError:
+    print("Vous n'avez pas saisie de nombre.")
+except AssertionError:
+    print("L'année saisie est inférieure ou égale à 0")
+
+if annee%400 == 0 or (annee%4 == 0 and annee%100 !=0):
+    print("L'année saisie est bissextile.")
+else:
+    print("L'année saisie n'est pas bissextile.")
+
